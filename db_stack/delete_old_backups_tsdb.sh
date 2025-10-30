3bc2127107ab33d2699223f38b35d30e90449bb4
bueno <bueno@bb.b>
2025-10-30 10:09:43 +0100

    moved db stack to seperate repo for future reuse

--- db_stack/delete_old_backups_tsdb.sh
+++
@@ -1,7 +0,0 @@
-BACKUP_DIR="/var/backups/timescaledb"  
-RETENTION_DAYS=30  
-
-log "Cleaning up backups older than ${RETENTION_DAYS} days"  
-find "${BACKUP_DIR}" -name "backup_*.sql.gz" -mtime +${RETENTION_DAYS} -delete  
-find "${BACKUP_DIR}" -name "backup_*.log" -mtime +${RETENTION_DAYS} -delete  
-log "Cleaning done"  
